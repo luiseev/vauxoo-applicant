be3811e03e07258be3bcd7eb5b627cbb659ee105
Luis Escobar <luiseev@gmail.com>
2015-11-25 00:19:11 -0430

    sql2

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,6 +3,13 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
+CREATE TABLE employee_hobby (
+    id INT, 
+    name VARCHAR(30),
+    description VARCHAR(100),
+    PRIMARY KEY(id)
+);
+
 CREATE TABLE employee_department (
     id INT, 
     name VARCHAR(30),
@@ -19,6 +26,14 @@ CREATE TABLE employee (
     FOREIGN KEY (id_dep) REFERENCES employee_department(id)
 );
 
+CREATE TABLE employees_hobbies_rel (
+    id_emp INT,
+    id_hobby INT,
+    PRIMARY KEY (id_emp, id_hobby),
+    FOREIGN KEY (id_emp) REFERENCES employee(id),
+    FOREIGN KEY (id_hobby) REFERENCES employee_hobby(id)
+);
+
 INSERT INTO employee_department VALUES(1,'Talento Humano','Departamento encargado del control del capital humano de la empresa');
 INSERT INTO employee_department VALUES(2,'Contabilidad y Finanzas', 'Departamento encargado del manejo de las finanzas de la empresa');
 INSERT INTO employee_department VALUES(3, 'Ventas', ' Departamento encargado de las ventas de los productos de la empresa');
@@ -31,7 +46,16 @@ INSERT INTO employee VALUES(2,2,'Juan','Gonzalez');
 INSERT INTO employee VALUES(3,3,'Maria','Martinez');
 INSERT INTO employee VALUES(4,5,'Luis','Escobar');
 
--- CREATE TABLE employee_hobby (
--- );
+INSERT INTO employee_hobby VALUES(1, 'Ping Pong', 'Jugar Ping Pong en las horas Libres');
+INSERT INTO employee_hobby VALUES(2, 'Ajedrez', 'Jugar Ajedrez');
+INSERT INTO employee_hobby VALUES(3, 'GYM', 'Ir al GYM');
 
+INSERT INTO employees_hobbies_rel VALUES(1,1);
+INSERT INTO employees_hobbies_rel VALUES(1,2);
+INSERT INTO employees_hobbies_rel VALUES(2,3);
+INSERT INTO employees_hobbies_rel VALUES(2,2);
+INSERT INTO employees_hobbies_rel VALUES(3,3);
+INSERT INTO employees_hobbies_rel VALUES(3,2);
+INSERT INTO employees_hobbies_rel VALUES(4,1);
+INSERT INTO employees_hobbies_rel VALUES(4,2);
 -- ...
