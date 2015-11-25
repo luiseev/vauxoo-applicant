1f23a197a5dcbf2d532acf1b5b38b98abcc7aeba
Luis Escobar <luiseev@gmail.com>
2015-11-24 23:58:57 -0430

    sql1

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,13 +3,35 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
-);
-
 CREATE TABLE employee_department (
+    id INT, 
+    name VARCHAR(30),
+    description VARCHAR(100),
+    PRIMARY KEY(id)
 );
 
-CREATE TABLE employee_hobby (
+CREATE TABLE employee (
+    id INT, 
+    id_dep INT, 
+    first_name VARCHAR(30), 
+    last_name VARCHAR(30),
+    PRIMARY KEY(id),
+    FOREIGN KEY (id_dep) REFERENCES employee_department(id)
 );
+
+INSERT INTO employee_department VALUES(1,'Talento Humano','Departamento encargado del control del capital humano de la empresa');
+INSERT INTO employee_department VALUES(2,'Contabilidad y Finanzas', 'Departamento encargado del manejo de las finanzas de la empresa');
+INSERT INTO employee_department VALUES(3, 'Ventas', ' Departamento encargado de las ventas de los productos de la empresa');
+INSERT INTO employee_department VALUES(4, 'Soporte Tecnico', 'Departamento encargado del soporte interno al usuario');
+INSERT INTO employee_department VALUES(5, 'Sistemas', 'Departamento encargado del desarrollo y mantenimiento de sistemas de información');
+INSERT INTO employee_department VALUES(6, 'Almacen', 'Departamento encargado del manejo de los productos en almacen');
+
+INSERT INTO employee VALUES(1,1,'Pedro','Perez');
+INSERT INTO employee VALUES(2,2,'Juan','Gonzalez');
+INSERT INTO employee VALUES(3,3,'Maria','Martinez');
+INSERT INTO employee VALUES(4,5,'Luis','Escobar');
+
+-- CREATE TABLE employee_hobby (
+-- );
 
 -- ...
